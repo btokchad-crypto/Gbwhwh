a5c760769c4fbc836e7c9d289fa03572e9434916
btokchad-crypto <btokchad@gmail.com>
2026-09-15 23:25:53 +0100

    Update snipe.js

--- snipe.js
+++ snipe.js
@@ -1,5 +1,5 @@
 // ============================================
-//WTF TRADING BOT - Complete Implementation
+//Fom TRADING BOT - Complete Implementation
 // with Debug Logging & Professional Messages 
 // ============================================
 import { Telegraf, Markup } from 'telegraf';
